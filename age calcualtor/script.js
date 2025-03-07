c206336718e7cbf2f48ef89e2424a8b559557180
joseph <abhulimenjoseph182@gmail.com>
2025-03-07 18:31:12 +0000

    Second Project

---
+++ age calcualtor/script.js
@@ -0,0 +1,18 @@
+let dob = document.getElementById('dob');
+let currDate = document.getElementById('currDate');
+let calAge = document.getElementById('calAge');
+let displayAge = document.getElementById('displayage');
+let age = document.getElementById('age');
+let today = new Date();
+currDate.innerHTML =`Today's Date is :${today.toLocaleDateString("en-US")}`;
+
+calAge.addEventListener('click', ()=>{
+    let dobValue = new Date(dob.value);
+    let ageValue = today.getFullYear() - dobValue.getFullYear();
+    let month = today.getMonth() - dobValue.getMonth();
+    if(month < 0 || (month === 0 && today.getDate() < dobValue.getDate())){
+        ageValue--;
+    }
+    displayAge.innerHTML = `Your age is ${ageValue}`;
+    age.value = ageValue;
+});
